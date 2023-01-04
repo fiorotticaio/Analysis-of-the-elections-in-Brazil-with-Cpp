d7c9b2cf32aa7a6e8fc5d5b690e9ff17dc709ab9
fiorotticaio <caiofiorotti@gmail.com>
2023-01-04 10:10:41 -0300

    :sparkles: adding the template off leitor class

--- leitor.cpp
+++ leitor.cpp
@@ -1,1 +1,25 @@
 #include "leitor.h"
+
+void leitor::leArquivoCandidatos
+(
+    const string& caminhoArquivo,
+    const map<int, candidato>& candidatos,
+    const map<int, partido>& partidos,
+    const int& flag
+) {
+
+}
+
+void leitor::leArquivoVotacao
+(
+    const string& caminhoArquivo,
+    const map<int, candidato>& candidatos,
+    const map<int, partido>& partidos,
+    const int &flag
+) {
+
+}
+
+void leitor::adicionaCandidatosPartidos(const map<int, candidato>& candidatos, const map<int, partido>& partidos) {
+
+}
--- leitor.h
+++ leitor.h
@@ -1,1 +1,36 @@
+#ifndef LEITOR_H
+
+#include "candidato.h"
+#include "partido.h"
+#include <string>
+#include <map>
+#include <fstream>
+#include <iostream>
+#include <iostream>
+#include <iomanip>
+
 using namespace std;
+
+class leitor {
+
+
+public:
+    // Construtor implícito
+    void leArquivoCandidatos
+    (
+        const string& caminhoArquivo,
+        const map<int, candidato>& candidatos,
+        const map<int, partido>& partidos,
+        const int& flag
+    );
+    void leArquivoVotacao
+    (
+        const string& caminhoArquivo,
+        const map<int, candidato>& candidatos,
+        const map<int, partido>& partidos,
+        const int& flag
+    );
+    void adicionaCandidatosPartidos(const map<int, candidato>& candidatos, const map<int, partido>& partidos);  
+};
+
+#endif // !LEITOR_H
